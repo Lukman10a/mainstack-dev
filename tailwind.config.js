57247b6d462880c2e7d43a4fb168362ceb63fef7
Lukman10a <77152819+Lukman10a@users.noreply.github.com>
2023-04-24 23:42:53 +0100

    feat: finish integrating api for views graph, location and refferer

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sohne: ["Test Söhne", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
